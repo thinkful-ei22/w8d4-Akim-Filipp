9c79869d6b3edb475b974047e09e021fd1945d21
Akim Desire <adesi006.ng@gmail.com>
2018-08-02 14:01:28 -0400

    AutoLog out WIP

--- src/store.js
+++ src/store.js
@@ -3,7 +3,6 @@ import { reducer as formReducer } from "redux-form";
 import thunk from "redux-thunk";
 import authReducer from "./reducers/auth";
 import protectedDataReducer from "./reducers/protected-data";
-import { setAuthToken, refreshAuthToken } from "./actions/auth";
 
 const store = createStore(
   combineReducers({
